d1d8044b15bb20f4d1305bd0d2fa7a5ec9ac6f1a
yucx <yucx@visionet.com.cn>
2018-12-05 17:11:27 +0800

    dev

---
+++ src/main/java/com/visionet/config/TestConfig.java
@@ -0,0 +1,13 @@
+package com.visionet.config;
+
+/**
+ * @ Author     ：yucx
+ * @ Date       ：Created in 下午5:10 2018/12/5
+ * @ Version    ：1.0
+ */
+public class TestConfig {
+
+    public static void main(String[] args) {
+
+    }
+}
