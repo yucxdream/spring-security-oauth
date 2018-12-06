d460728fc4c8d00bd6fb33aa9760c51b6a0731b2
yucx <yucx@visionet.com.cn>
2018-12-06 14:50:33 +0800

    master分支上添加gggggg

--- src/main/java/com/visionet/config/TestConfig.java
+++ src/main/java/com/visionet/config/TestConfig.java
@@ -14,5 +14,6 @@ public class TestConfig {
         System.out.println("cccccg");
         System.out.println("dddddd");
         System.out.println("eeeeee");
+        System.out.println("gggggg");
     }
 }
