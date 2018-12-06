133fd2837d3fd0e0421a6cf6652879f7a60d79df
yucx <yucx@visionet.com.cn>
2018-12-06 15:15:25 +0800

    测试1

--- src/main/java/com/visionet/config/TestConfig.java
+++ src/main/java/com/visionet/config/TestConfig.java
@@ -15,5 +15,6 @@ public class TestConfig {
         System.out.println("dddddd");
         System.out.println("eeeeee");
         System.out.println("ffffff");
+        System.out.println("yucx2");
     }
 }
